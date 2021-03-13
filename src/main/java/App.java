47af495c317e3d85f21b47368c556ec91ea657bc
Antia <antiap@gmail.com>
2021-03-13 13:58:41 +0100

    kaka

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,5 +5,7 @@ import ventanas.VentanaPrincipal;
 public class App {
     public static void main(String[] args) {
         VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+
+	//kaka
     }
 }
