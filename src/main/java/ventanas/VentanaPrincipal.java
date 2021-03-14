f686b1d7f24d4db6c7c88d73cd2c42475ece7a4f
Manu <manuelfv366@hotmail.com>
2021-03-14 21:37:50 +0100

    ajustar tamano de los botones en el pPrincipal

--- src/main/java/ventanas/VentanaPrincipal.java
+++ src/main/java/ventanas/VentanaPrincipal.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import ventanas.paneles.PPrincipal;
 import java.awt.*;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class VentanaPrincipal extends JFrame {
     PPrincipal pPrincipal = new PPrincipal();
@@ -46,7 +44,7 @@ public class VentanaPrincipal extends JFrame {
     private void iniciarComponentes(){
         colocarPaneles();
         // colocarEtiquetas();
-        colocarBotones();
+        // colocarBotones();
     }
 
     private void crearMenu(){
@@ -61,43 +59,4 @@ public class VentanaPrincipal extends JFrame {
         this.getContentPane().add(pPrincipal);
     }
 
-    private void colocarBotones(){
-
-        JButton boton1 = new JButton();
-        boton1.setSize(getSize().height/2, getSize().width/8);
-        boton1.setLocation(getSize().height/4, getSize().width/8);
-        ImageIcon deporteImg = new ImageIcon("src/main/java/ventanas/deporte1.jpg");
-        boton1.setIcon(new ImageIcon(deporteImg.getImage().getScaledInstance(boton1.getWidth(), boton1.getHeight(), Image.SCALE_SMOOTH)));
-        pPrincipal.add(boton1);
-        
-        ActionListener listener = new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent ae){
-                VentanaDeporte ventanaDeporte = new VentanaDeporte();
-                
-            }
-        };
-        boton1.addActionListener(listener);
-
-
-        JButton boton2 = new JButton();
-        boton2.setSize(getSize().height/2, getSize().width/8);
-        boton2.setLocation(getSize().height - getSize().height/20, getSize().width/8);
-        ImageIcon conciertoImg = new ImageIcon("src/main/java/ventanas/musica.jpg");
-        boton2.setIcon(new ImageIcon(conciertoImg.getImage().getScaledInstance(boton2.getWidth(), boton2.getHeight(), Image.SCALE_SMOOTH)));
-
-        pPrincipal.add(boton2);
-
-        JButton boton3 = new JButton();
-        boton3.setSize(getSize().height/2, getSize().width/8);
-        boton3.setLocation(getSize().height - getSize().height/20, getSize().width/3);
-
-        pPrincipal.add(boton3);
-
-        JButton boton4 = new JButton();
-        boton4.setSize(getSize().height/2, getSize().width/8);
-        boton4.setLocation(getSize().height/4, getSize().width/3);
-
-        pPrincipal.add(boton4);
-    }
 }
--- src/main/java/ventanas/paneles/PPrincipal.java
+++ src/main/java/ventanas/paneles/PPrincipal.java
@@ -7,13 +7,16 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Image;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.*;
 
 
 public class PPrincipal extends JPanel{
     
     public PPrincipal(){
         colocarEtiquetas();
-        // colocarBotones();
+        colocarBotones();
     }
 
     private void colocarEtiquetas(){
@@ -47,6 +50,51 @@ public class PPrincipal extends JPanel{
         // etiMusica.setBackground(Color.BLACK);
         this.add(etiMusica);
 
+    }
+
+    private void colocarBotones(){
+
+        Toolkit miPantalla = Toolkit.getDefaultToolkit();
+        Dimension tamanoPantalla = miPantalla.getScreenSize();
+        int alturaPantalla = tamanoPantalla.height;
+        int anchoPantalla = tamanoPantalla.width;
+
+        JButton boton1 = new JButton();
+        boton1.setSize(alturaPantalla/2, anchoPantalla/8);
+        boton1.setLocation(alturaPantalla/4, anchoPantalla/8);
+        ImageIcon deporteImg = new ImageIcon("src/main/java/ventanas/deporte1.jpg");
+        boton1.setIcon(new ImageIcon(deporteImg.getImage().getScaledInstance(boton1.getWidth(), boton1.getHeight(), Image.SCALE_SMOOTH)));
+        add(boton1);
+        
+        ActionListener listener = new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                VentanaDeporte ventanaDeporte = new VentanaDeporte();
+                
+            }
+        };
+        boton1.addActionListener(listener);
+
+
+        JButton boton2 = new JButton();
+        boton2.setSize(alturaPantalla/2, anchoPantalla/8);
+        boton2.setLocation(alturaPantalla - alturaPantalla/20, anchoPantalla/8);
+        ImageIcon conciertoImg = new ImageIcon("src/main/java/ventanas/musica.jpg");
+        boton2.setIcon(new ImageIcon(conciertoImg.getImage().getScaledInstance(boton2.getWidth(), boton2.getHeight(), Image.SCALE_SMOOTH)));
+
+        add(boton2);
+
+        JButton boton3 = new JButton();
+        boton3.setSize(alturaPantalla/2, anchoPantalla/8);
+        boton3.setLocation(alturaPantalla - alturaPantalla/20, anchoPantalla/3);
+
+        add(boton3);
+
+        JButton boton4 = new JButton();
+        boton4.setSize(alturaPantalla/2, anchoPantalla/8);
+        boton4.setLocation(alturaPantalla/4, anchoPantalla/3);
+
+        add(boton4);
     }
 
 }
