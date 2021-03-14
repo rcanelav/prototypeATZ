6b2c05312b6654950cdafef3641bf06299914b5d
Ramon <r.canelav@gmail.com>
2021-03-14 04:31:47 -0700

    prueba

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,8 @@ public class App {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
+
+		//prueba
             }
         });
 
